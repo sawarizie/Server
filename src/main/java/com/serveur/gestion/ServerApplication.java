8f6718c17eab1dfe8949db5fc24247d1b0d83118
sawarizie <sawariziefelix@gmail.com>
2020-04-20 16:09:45 +0000

    moi

---
+++ src/main/java/com/serveur/gestion/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.serveur.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
